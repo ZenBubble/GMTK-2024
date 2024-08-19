c253872e622b1b0cf22ef1c8f85b4547ec81a090
Minghao Li <52687312+Lmh-java@users.noreply.github.com>
2024-08-20 01:35:04 +0800

    Add a new level (#83)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -143,7 +143,8 @@ public class GameManager : MonoBehaviour
     /// <exception cref="NotImplementedException"></exception>
     private void HandleWin()
     {
-        SwitchLevel(_level.NextLevel().ToIndex());
+        _level = _level.NextLevel();
+        SwitchLevel(_level.ToIndex());
     }
 
     /// <summary>
